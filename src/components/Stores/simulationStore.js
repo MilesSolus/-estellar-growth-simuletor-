db66a2daf3d688e3aadd9d8df5853f6d4e13aa4b
MilesSolus <vulgoroo@gmail.com>
2025-06-22 06:20:51 -0300

    Update simulationStore.js

--- src/components/Stores/simulationStore.js
+++ src/components/Stores/simulationStore.js
@@ -1,1 +1,19 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
+export const useSimulationStore = defineStore('simulation', () => {
+  const simulation = ref({
+    assets: [],
+    monthlyAmount: 0,
+    term: 12,
+    scenario: 'optimistic'
+  })
+
+  const historicalData = ref([])
+
+  const fetchHistoricalData = async () => {
+    // API call implementation
+  }
+
+  return { simulation, historicalData, fetchHistoricalData }
+})
