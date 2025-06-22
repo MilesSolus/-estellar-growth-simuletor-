58e0af1ba1bbee8d10fc44ba2d61c8f202448d2c
MilesSolus <vulgoroo@gmail.com>
2025-06-22 06:22:19 -0300

    Update useCalculation.js

--- src/components/composable/useCalculation.js
+++ src/components/composable/useCalculation.js
@@ -1,1 +1,12 @@
+import { computed } from 'vue'
+import { useSimulationStore } from '../stores/simulationStore'
 
+export function useCalculation() {
+  const store = useSimulationStore()
+  
+  const projectedValue = computed(() => {
+    // Complex calculation logic
+  })
+
+  return { projectedValue }
+}
